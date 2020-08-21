09deace4f3fa015d5e275d3fb6124ce83ac909f8
jjpenad <60227671+jjpenad@users.noreply.github.com>
2020-08-21 15:55:32 -0500

    Taller2 completado

--- measurements/models.py
+++ measurements/models.py
@@ -5,8 +5,8 @@ from variables.models import Variable
 class Measurement(models.Model):
     variable = models.ForeignKey(Variable, on_delete=models.CASCADE)
     value = models.FloatField(null=True, blank=True, default=None)
-    unit= models.CharField(max_length=50)
-    place =models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
     dateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
